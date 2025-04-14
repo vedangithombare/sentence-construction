da15abeaa5030bc8e7a0c91572e05de265b08c94
Vedangi <tvedangi@gmail.com>
2025-04-14 13:17:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,61 @@
 import Header from "./components/Header.jsx";
 import Introduction from "./components/Introduction.jsx";
 import SentenceConstruction from "./components/SentenceConstruction.jsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Results from "./components/Results.jsx";
+import data from "./data.json";
 
 const MAX_TIMEOUT = 30;
 
 function App() {
-    const [hasStarted, setHasStarted] = useState(false);
-    const [submissions, setSubmissions] = useState([]);
-    const [response, setResponse] = useState({});
-    const [hasFinished, setHasFinished] = useState(false);
+  const [hasStarted, setHasStarted] = useState(false);
+  const [submissions, setSubmissions] = useState([]);
+  const [response, setResponse] = useState({});
+  const [hasFinished, setHasFinished] = useState(false);
 
-    const handleSubmissions = (results) => {
-        setSubmissions(results);
-        setHasFinished(!hasFinished);
-        setHasStarted(!hasStarted);
+  const handleSubmissions = (results) => {
+    setSubmissions(results);
+    setHasFinished(!hasFinished);
+    setHasStarted(!hasStarted);
+  };
+
+  useEffect(() => {
+    try {
+      const questionData = data.getSentenceConstruction;
+      setResponse(questionData);
+    } catch (error) {
+      console.log(error);
     }
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:5000/getSentenceConstruction")
-            .then(async (res) => {
-                const d = await res.json();
-                setResponse(d);
-            })
-            .catch(err => console.log(err));
-    }, []);
+  console.log(submissions);
 
-    console.log(submissions);
-
-    return <>
-        {
-            hasStarted ?
-                <SentenceConstruction questions={response?.data.questions} setSubmissions={handleSubmissions}/> :
-                <>
-                    <Header hasFinished={hasFinished} setHasFinished={setHasFinished} />
-                    {
-                        hasFinished ?
-                            <Results questions={response?.data.questions} submissions={submissions}/> :
-                            <Introduction maxTime={MAX_TIMEOUT} data={response} setHasStarted={setHasStarted}/>
-                    }
-                </>
-        }
+  return (
+    <>
+      {hasStarted ? (
+        <SentenceConstruction
+          questions={response?.data.questions}
+          setSubmissions={handleSubmissions}
+        />
+      ) : (
+        <>
+          <Header hasFinished={hasFinished} setHasFinished={setHasFinished} />
+          {hasFinished ? (
+            <Results
+              questions={response?.data.questions}
+              submissions={submissions}
+            />
+          ) : (
+            <Introduction
+              maxTime={MAX_TIMEOUT}
+              data={response}
+              setHasStarted={setHasStarted}
+            />
+          )}
+        </>
+      )}
     </>
+  );
 }
 
 export { MAX_TIMEOUT };
